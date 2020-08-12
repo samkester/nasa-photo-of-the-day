13aaebeb0ef93afa836c52cc9d5da8ff0dfbe0dc
Sam Kester <samkester@gmail.com>
2020-08-12 13:58:15 -0600

    project outline; initial setup of App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+import ContentBox from "./components/ContentBox"
+import DateButton from "./components/DateButton"
+
+// App structure:
+// App - state: date to fetch, result of API call
+//   ContentBox - prop: data from API call
+//     ImageBox - props: src, media type
+//   DateButton - props: date to fetch, remaining API calls/hr or day
+
+// API should call on load and when date to fetch changes
 
 function App() {
   return (
