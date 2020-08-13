15b94a08c1c7546ed2b6390f5cc42107cbc5f8e7
Sam Kester <samkester@gmail.com>
2020-08-13 14:36:18 -0600

    ImageBox styles

--- src/components/ImageBox.js
+++ src/components/ImageBox.js
@@ -1,12 +1,26 @@
 //     ImageBox - props: src, media type, title
 
 import React from "react";
+import styled from "styled-components";
+
+const StyledImageBox = styled.div`
+    font-size: 2rem;
+    width: 100%;
+    ${props => props.theme.flexCenter}
+    margin-bottom: 1%;
+    /*border-radius: ${props => props.theme.componentCorners};
+    overflow: hidden;*/
+    
+    img{
+        width: 100%;
+    }
+`
 
 export default function(props){
     const {src, media_type, title} = props;
 
     return(
-        <div className="image-box">
+        <StyledImageBox>
             {
                 !src ?
                 <p>No image loaded, please wait.</p>: // src = blank or undefined
@@ -14,6 +28,6 @@ export default function(props){
                 <p>Cannot load this item. Please select a different date.</p> : // not an image
                 <img src={src} alt={title} />
             }
-        </div>
+        </StyledImageBox>
     );
 }
