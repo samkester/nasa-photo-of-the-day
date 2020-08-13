1ae592052381a37dcd8d6a1c26f1a5dbcd1de211
Sam Kester <samkester@gmail.com>
2020-08-13 13:42:27 -0600

    add'l setup to fix theme bugs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import ThemeProvider from "styled-components";
+import {ThemeProvider} from "styled-components";
 import theme from "./theme"
 
 ReactDOM.render(
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,9 @@
 const theme = {
-    backgroundColor = "#00002f",
-    mainColor = "#c9c",
-    highlightColor = "#93c"
+    backgroundColor: "#00002f",
+    mainColor: "#c9c",
+    highlightColor: "#93c",
+    componentMargin: "2% 5%",
+    componentCorners: "1rem",    
 }
 
 export default theme;
